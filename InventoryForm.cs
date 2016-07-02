c5f21c4b46c18ce5b1bc80470879ba863035a9cd
Derek O'Brien <4djobrien@gmail.com>
2016-07-02 16:26:56 -0400

    Inventory form
    Will allow user to browse, insert, remove, or modify vehicles in the inventory database. Uses detailsview.

---
+++ InventoryForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class frmInventory : Form
+    {
+        public frmInventory()
+        {
+            InitializeComponent();
+        }
+
+        private void inventoryBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.inventoryBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.inventoryDBDataSet);
+
+        }
+
+        private void frmInventory_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'inventoryDBDataSet.Inventory' table. You can move, or remove it, as needed.
+            this.inventoryTableAdapter.Fill(this.inventoryDBDataSet.Inventory);
+
+        }
+
+    }
+}
